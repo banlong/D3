8c2c4b18f52fce2bbd22abfb5531857932724ad6
NGHIEP <nghiepnds@yahoo.com>
2016-10-19 08:42:03 -0700

    Add computeTextLength to align text with the bar

--- js/div.js
+++ js/div.js
@@ -8,34 +8,60 @@
     drawSVG();
     drawBarChart();
 
-    function drawBarChart(){
+    function drawScatter(){
+
+    }
+
+    function drawBarChart() {
         //var dataset = [ 5, 10, 15, 20, 25 ];
-        var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+        var dataset = [23, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
         var w = 500;
-        var h = 100;
+        var h = 150;
         var barPadding = 1;
+        var barWidth = w / dataset.length - barPadding;
+        //Create SVG area
         var svg = d3.select("body")
             .append("svg")
             .attr("width", w)
             .attr("height", h);
 
+        //Add bars
         var rects = svg.selectAll("rect")
             .data(dataset)
             .enter()
             .append("rect")
-            .attr("x",function(d, i) {
+            .attr("x", function (d, i) {
                 return i * (w / dataset.length);
             })
-            .attr("y", function(d) {
-                return (h-4*d);
+            .attr("y", function (d) {
+                return (h - 4 * d);
             })
-            .attr("width", w / dataset.length - barPadding)
-            .attr("height", function(d) {
-                return (4*d);
+            .attr("width", barWidth)
+            .attr("height", function (d) {
+                return (4 * d);
             })
-            .attr("fill", function(d) {
-                return "rgb(0, 0, " + (d * 10) + ")";
+            .attr("fill", function (d) {
+                return "rgb(5, 134, " + (d * 10) + ")";
             });
+
+        //Add text to chart
+        var textTopPad = 1;
+        var texts = svg.selectAll("text")
+            .data(dataset)
+            .enter()
+            .append("text")
+            .text(function (d) {
+                return d;
+            })
+            .attr("x", function (d, i) {
+                var textWidth = this.getComputedTextLength();       //get text box length
+                var textLeftPad =  (barWidth - textWidth)/2;        //text padding
+                return i * (w / dataset.length) + textLeftPad;      //x-position of text
+            })
+            .attr("y", function (d) {
+                return h - (d * 4)-textTopPad;
+            });
+
     }
 
     function drawDiv(){
